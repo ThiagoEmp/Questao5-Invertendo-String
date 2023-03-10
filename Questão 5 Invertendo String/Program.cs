f75b87be0039b7715cb48e8c48995d26589375e4
Thiag <Thiag@DESKTOP-43REEAS>
2023-03-10 16:51:20 -0300

    Adicionar arquivos de projeto.

---
+++ Questão 5 Invertendo String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    //Questão 5 - Invertendo String
+    static void Main()
+    {
+
+        string caracteres = "";
+        string inversao = "";
+
+        Console.WriteLine("Insira uma sequência de caracteres: ");
+        caracteres = Console.ReadLine();
+
+        for (int i = caracteres.Length - 1; i >= 0; i--)
+        {
+            inversao += caracteres[i];
+        }
+
+        Console.WriteLine(inversao);
+
+
+        Console.ReadLine();
+    }
+}
